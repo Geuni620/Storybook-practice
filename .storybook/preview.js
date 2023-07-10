be382f41c7a079883a4a484c65aef8954dfbf3ff
Geuni <dlrmsgnl620@naver.com>
2023-07-10 23:32:56 +0900

    Add: docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,32 @@
-/** @type { import('@storybook/react').Preview } */
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Controls,
+  Stories,
+} from "@storybook/blocks";
+
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {argTypesRegex: "^on[A-Z].*"},
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories />
+        </>
+      ),
     },
   },
 };
--- src/Hello/Hello.stories.js
+++ src/Hello/Hello.stories.js
@@ -2,15 +2,18 @@ import React from "react";
 import {action} from "@storybook/addon-actions";
 import Hello from "./Hello";
 
+// import mdx from "./Hello.mdx";
+
 export default {
   title: "components|basic/Hello", // 스토리북에서 보여질 그룹과 경로를 명시
   component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
-
+  tags: ["autodocs"],
   parameters: {
     componentSubtitle: '"안녕하세요"라고 보여주는 컴포넌트',
   },
 };
-export const standard = ({title}) => (
+
+export const standard = () => (
   <Hello
     name="Storybook"
     onHello={action("hello clicked")}
